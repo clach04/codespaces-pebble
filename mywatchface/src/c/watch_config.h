1f9b31f2634dd0862ad6acdea8ac1474c078bfb8
clach04 <clach04@gmail.com>
2025-11-15 22:42:11 -0800

    Import of 2025-04-13 https://github.com/clach04/pebble_watchface_framework/commit/eed74388b6f4834452426222ed15f82a89c5146e

---
+++ mywatchface/src/c/watch_config.h
@@ -0,0 +1,56 @@
+#pragma once
+
+#define USE_GENERIC_MAIN
+#undef REMOVE_LEADING_ZERO_FROM_TIME  /* 12 hour display will not have a leading '0' or ' ' if this is defined */
+#undef FONT_NAME
+#undef FONT_SYSTEM_NAME  /* the default font system will be used */
+#undef DEBUG_TIME
+//#define USE_TIME_MACHINE  // NOTE mixing with DEBUG_TIME doesn't make sense. Requires update to package.json for dependencies
+
+// Show step count using builtin code
+#define USE_HEALTH
+//#define UPDATE_HEALTH_ON_ACTIVITY  /* If not set, only updates step count display once per minute */
+
+#define DRAW_BATTERY
+#define DRAW_SMALL_BATTERY
+
+//#define QUIET_TIME_IMAGE RESOURCE_ID_IMAGE_QUIET_TIME
+//#define QUIET_TIME_IMAGE_GRECT GRect(20, 20, 20, 20)  // Example assumes a 20x20 image
+
+#ifdef PBL_ROUND /* 180x180 */
+/*TODO center/move right*/
+    #define CLOCK_POS GRect(0, 52, 180, 180) /* probably taller than really needed */
+    #define HEALTH_POS GRect(0, 40, 180, 180)
+    #define BT_POS GRect(0, 100, 180, 180) /* probably taller than really needed */
+
+    #define DATE_ALIGN GTextAlignmentCenter
+    #define DATE_POS GRect(0, 120, 180, 180) /* probably taller than really needed */
+
+    #define BAT_ALIGN GTextAlignmentCenter
+    #ifdef DRAW_BATTERY
+        #define BAT_POS GRect(85, 10, 180, 180) /* probably taller than really needed */
+    #else
+        #define BAT_POS GRect(0, 140, 180, 180) /* probably taller than really needed */
+    #endif /* DRAW_BATTERY */
+
+#else /* PBL_RECT 144x168*/
+    #define CLOCK_POS GRect(0, 52, 144, 168) /* probably taller than really needed */
+    #define HEALTH_POS GRect(0, 40, 144, 168)
+    #define BT_POS GRect(0, 120, 144, 168) /* probably taller than really needed */
+    #define DATE_POS GRect(0, 140, 144, 168) /* probably taller than really needed */
+    #ifdef DRAW_BATTERY
+        #define BAT_POS GRect(5, 150, 144, 168)
+    #else
+        #define BAT_POS GRect(0, 140, 144, 168) /* probably taller than really needed */
+    #endif /* DRAW_BATTERY */
+#endif /* end of Round or rectangle */
+
+/* for screen shots and font testing
+#define DEBUG_TIME
+#define DEBUG_TIME_SCREENSHOT
+// ensure quiet time and bluetooth disconnection info is shown
+#ifndef quiet_time_is_active  // so not aplite
+#define quiet_time_is_active() true  // DEBUG!
+#endif
+#define bluetooth_connection_service_peek() false  // DEBUG!
+*/
